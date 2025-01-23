a11157f74928ac399ab07bb13194ca8a05ff118e
20161609 <dksshwns@naver.com>
2025-01-23 13:38:15 +0900

    ipynb file(18_cnn_bin_cats_dogs_transfer) for studying Convolutional Neural Network.

--- generated.py
+++ generated.py
@@ -8,55 +8,89 @@ import seaborn as sns
 
 import tensorflow as tf
 import keras
-from keras.utils import to_categorical
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
+from keras import layers
 
-from keras.datasets import fashion_mnist
+import requests
+import zipfile
+import io
+import PIL
+import os
 
-data = fashion_mnist.load_data()
 
-(X_train, y_train), (X_test, y_test) = data
-X_train.shape, y_train.shape, X_test.shape, y_test.shape
+# Data URL
+data_url = 'https://raw.githubusercontent.com/20161609/data_box/main/cats_and_dogs.zip'
+response = requests.get(data_url)
+if response.status_code == 200:
+    zip_data = io.BytesIO(response.content)  # Processing in memory
+    with zipfile.ZipFile(zip_data, 'r') as zip_ref:
+        zip_ref.extractall('/content')  # Decompress in memory
 
-columns = ['T-shirt/top', 'Trouser',  'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    data_root = '/content/cats_and_dogs'
+    train_dir = data_root + '/train'
+    test_dir = data_root + '/test'
+
+    # Load the training data
+    train_cats_fnames = os.listdir(train_dir + '/cats')
+    train_dogs_fnames = os.listdir(train_dir + '/dogs')
+else:
+    raise Exception(f"Failed to download data. Status code: {response.status_code}")
+
+class_name = ['cats', 'dogs']
+X_train, y_train = [], []
+
+for fname in train_cats_fnames:
+    image = PIL.Image.open(train_dir + '/cats/' + fname)
+    image = image.resize((224, 224))
+    arr = np.array(image)
+    X_train.append(arr)
+    y_train.append(0)
+
+for fname in train_dogs_fnames:
+    image = PIL.Image.open(train_dir + '/dogs/' + fname)
+    image = image.resize((224, 224))
+    arr = np.array(image)
+    X_train.append(arr)
+    y_train.append(1)
+
+len(X_train), len(y_train)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+print(type(X_train), type(y_train))
+print(X_train.shape, y_train.shape)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2,
+                                                  shuffle=True,
+                                                  random_state=42)
+
+print(X_train.shape, X_val.shape)
+print(y_train.shape, y_val.shape)
 
 # np.random.seed(42)
-sample = np.random.randint(60000, size=25)
+sample = np.random.randint(2080, size=25)
 
 fig = plt.figure(figsize=(8, 8))
 for i, idx in enumerate(sample):
     plt.subplot(5, 5, i+1)
     plt.imshow(X_train[idx], cmap='gray')
     plt.axis('off')
-    plt.title(columns[y_train[idx]])
+    plt.title(class_name[y_train[idx]])
 fig.tight_layout()
 plt.show()
 
-pd.Series(y_train).value_counts().sort_index()
-
-from sklearn.model_selection import train_test_split
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-
-X_train.shape, X_val.shape, y_train.shape, y_val.shape
-
-X_train_s = X_train / 255.
-X_val_s = X_val / 255.
-
-X_train_s = X_train_s.reshape(-1, 28, 28, 1)
-X_val_s = X_val_s.reshape(-1, 28, 28, 1)
+X_train_s = X_train.astype('float')/255.
+X_val_s = X_val.astype('float')/255.
 
 X_train_s.shape, X_val_s.shape
-
-y_train_o = to_categorical(y_train)
-y_val_o = to_categorical(y_val)
-
-y_train_o.shape, y_val_o.shape
 
 from keras import layers
 
 model = keras.Sequential([
     layers.Conv2D(filters=16, kernel_size=3, activation='relu',
-                  input_shape=(28, 28, 1)),
+                  input_shape=(224, 224, 3)),
     layers.MaxPooling2D(),
     layers.Dropout(0.2),
     layers.Conv2D(filters=32, kernel_size=3, activation='relu'),
@@ -64,26 +98,50 @@ model = keras.Sequential([
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
     layers.Dropout(0.2),
-    layers.Dense(10, activation='softmax')
+    layers.Dense(1, activation='sigmoid')
 ])
+
+model = keras.Sequential()
+model.add(layers.Conv2D(filters=16, kernel_size=3, activation='relu',
+                  input_shape=(224, 224, 3)))
+model.add(layers.MaxPooling2D())
+model.add(layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+model.add(layers.MaxPooling2D())
+model.add(layers.Conv2D(filters=64, kernel_size=3, activation='relu'))
+model.add(layers.MaxPooling2D())
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dropout(0.2))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
 
 model.summary()
 
-keras.utils.plot_model(model, dpi=72)
-
 model.compile(
-    loss="categorical_crossentropy",
-    optimizer="adam",
-    metrics=['accuracy'])
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
+)
 
-EPOCHS = 30
+from keras import callbacks
+
+es = callbacks.EarlyStopping(monitor="val_loss",patience=5)
+ckpt_path = './temp/cats_dogs.weights.h5'
+ckpt = keras.callbacks.ModelCheckpoint(
+    ckpt_path,
+    monitor="val_loss",
+    save_best_only=True,
+    save_weights_only=True
+)
+
+EPOCHS = 20
 BATCH_SIZE = 32
 
-history = model.fit(X_train_s, y_train_o,
+history = model.fit(X_train_s, y_train,
                     epochs=EPOCHS,
                     batch_size=BATCH_SIZE,
-                    validation_data=(X_val_s, y_val_o))
-
+                    callbacks=[es, ckpt],
+                    validation_data=(X_val_s, y_val))
 
 def plot_history(history):
     hist = pd.DataFrame(history.history)
@@ -109,34 +167,54 @@ def plot_history(history):
 
 plot_history(history)
 
-X_test_s = X_test / 255.
-X_test_s = X_test_s.reshape(-1, 28, 28, 1)
-y_test_o = to_categorical(y_test)
+model.load_weights(ckpt_path)
 
-y_pred = model.predict(X_test_s)
+# Data preprocessing
+test_cats_fnames = os.listdir(test_dir + '/cats')
+test_dogs_fnames = os.listdir(test_dir + '/dogs')
 
-y_pred = np.argmax(y_pred, axis=1)
-y_pred
+X_test = []
+y_test = []
 
-# Define a function to print metrics and display the confusion matrix
+for fname in test_cats_fnames:
+    image = PIL.Image.open(test_dir + '/cats/' + fname)
+    image = image.resize((224, 224))
+    arr = np.array(image)
+    X_test.append(arr)
+    y_test.append(0)
+
+for fname in test_dogs_fnames:
+    image = PIL.Image.open(test_dir + '/dogs/' + fname)
+    image = image.resize((224, 224))
+    arr = np.array(image)
+    X_test.append(arr)
+    y_test.append(1)
+
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+# Normalization of test data
+X_test_s = X_test.astype('float') / 255.
+
+X_test.shape, y_test.shape
+
+y_pred_proba = model.predict(X_test_s)
+y_pred = (y_pred_proba > 0.5).astype(int).flatten()
+
+y_pred.shape
+
+from sklearn.metrics import accuracy_score, recall_score, precision_score,f1_score
+from sklearn.metrics import confusion_matrix
+
 def print_metrics(y_true, y_pred, aver='binary'):
     print('accuracy:', accuracy_score(y_true, y_pred))
-    print('recall:', recall_score(y_true, y_pred, average=aver))
-    print('precision:', precision_score(y_true, y_pred, average=aver))
+    print('recall:', recall_score(y_true, y_pred ,average=aver))
+    print('precision:', precision_score(y_true, y_pred,average=aver))
     print('f1 :', f1_score(y_true, y_pred, average=aver))
 
-    # Plot the confusion matrix
     cfm = confusion_matrix(y_true, y_pred)
     s = sns.heatmap(cfm, annot=True, cmap='Blues', fmt='d', cbar=False)
     s.set(xlabel='Prediction', ylabel='Actual')
     plt.show()
 
-# Print metrics for the test dataset
 print_metrics(y_test, y_pred, aver='macro')
-
-
-model.save('cnn_muti_fashion.h5')
-
-model.save('cnn_muti_fashion.keras')
-
-# model.save('cnn_muti_fashion')
--- practice/extract_code_from_ipynb.py
+++ practice/extract_code_from_ipynb.py
@@ -26,7 +26,7 @@ def extract_code_from_ipynb(file_path, output_file):
     print(f"Code extracted to {output_file}")
 
 # Define the input .ipynb file path and the output .py file path
-FILE_PATH = '16_cnn_multi_fashionmnist.ipynb'  # Path to the Jupyter Notebook
+FILE_PATH = '17_cnn_bin_cats_dogs.ipynb'  # Path to the Jupyter Notebook
 OUTPUT_PATH = 'generated.py'  # Path for the generated Python file
 
 # Call the function to extract code
